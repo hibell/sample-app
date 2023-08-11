6f90af4e3be26fe616c1f7ff751c34d096f37eda
Hank W Ibell <hwibell@us.ibm.com>
2023-08-11 16:52:15 -0400

    Add unit tests

---
+++ src/test/java/com/ibm/appflow/BarTest.java
@@ -0,0 +1,15 @@
+package com.ibm.appflow;
+
+import org.junit.jupiter.api.Test;
+
+public class BarTest {
+    @Test
+    public void testBar1() {
+        System.out.println("testBar1 successful!");
+    }
+
+    @Test
+    public void testBar2() {
+        System.out.println("testBar2 successful!");
+    }
+}
---
+++ src/test/java/com/ibm/appflow/FooTest.java
@@ -0,0 +1,15 @@
+package com.ibm.appflow;
+
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+    @Test
+    public void testFoo1() {
+        System.out.println("testFoo1 successful!");
+    }
+
+    @Test
+    public void testFoo2() {
+        System.out.println("testFoo2 successful!");
+    }
+}
